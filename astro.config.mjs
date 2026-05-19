be2c45b31fb5ab86e8a71383831b06ef4f5ea3f4
Christy Montejo <christymontejo2003@gmail.com>
2026-05-19 14:58:05 +0800

    configure vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
